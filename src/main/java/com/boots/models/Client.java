9dc3680b5b2001d87a0ec8738a8c954fbbb06711
officerblunt <cuppetunbluze@gmail.com>
2022-03-31 19:40:15 +0300

    i'm so fucked up

---
+++ src/main/java/com/boots/models/Client.java
@@ -0,0 +1,76 @@
+package com.boots.models;
+
+public class Client {
+    int id;
+    String firstName;
+    String lastName;
+    String password;
+    String phone;
+    String organisation;
+    String address;
+
+    public Client(String firstName, String lastName, String password, String phone, String organisation) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.phone = phone;
+        this.organisation = organisation;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getOrganisation() {
+        return organisation;
+    }
+
+    public void setOrganisation(String organisation) {
+        this.organisation = organisation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
